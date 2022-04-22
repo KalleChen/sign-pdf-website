f9fd5a6f0d6e8d71aa6483e3d9e2c1b740e4d032
Kalle Chen <kallechen.tw@gmail.com>
2022-04-22 11:06:03 +0800

    Add custom tsa service

--- app.py
+++ app.py
@@ -23,110 +23,149 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 @app.route("/", methods=["POST", "GET"])
 def add_signature_and_timestamp():
-    if request.method == "POST":
-        if (
-            "pdf_file" not in request.files
-            or "cert_file" not in request.files
-            or "key_file" not in request.files
-        ):
-            return render_template("index.html", error="No file part")
-        pdf_file = request.files["pdf_file"]
-        key_file = request.files["key_file"]
-        cert_file = request.files["cert_file"]
-        if (
-            (pdf_file.filename == "" or pdf_file.filename is None)
-            or (cert_file.filename == "" or cert_file.filename is None)
-            or (key_file.filename == "" or key_file.filename is None)
-        ):
-            return render_template("index.html", error="No selected file")
-        if pdf_file and cert_file:
-            pdf_filename = "upload.pdf"
-            key_filename = "key.pem"
-            cert_filename = "cert.pem"
-            pdf_file.save(
-                os.path.join(app.config["UPLOAD_FOLDER"], pdf_filename)
-            )
-            key_file.save(
-                os.path.join(app.config["UPLOAD_FOLDER"], key_filename)
-            )
-            cert_file.save(
-                os.path.join(app.config["UPLOAD_FOLDER"], cert_filename)
-            )
-            signed_pdf_filename = sign_with_timestamp(
-                "./files/" + pdf_filename,
-                "./files/" + key_filename,
-                "./files/" + cert_filename,
-            )
-            return render_template(
-                "index.html",
-                file_url=url_for("download_file", filename=signed_pdf_filename),
-            )
+    try:
+        if request.method == "POST":
+            if (
+                "pdf_file" not in request.files
+                or "cert_file" not in request.files
+                or "key_file" not in request.files
+            ):
+                return render_template("index.html", error="No file part")
+            if "tsa_url" not in request.values:
+                return render_template("index.html", error="No tsa_url")
+            pdf_file = request.files["pdf_file"]
+            key_file = request.files["key_file"]
+            cert_file = request.files["cert_file"]
+            tsa_url = request.values["tsa_url"]
+            if (
+                (pdf_file.filename == "" or pdf_file.filename is None)
+                or (cert_file.filename == "" or cert_file.filename is None)
+                or (key_file.filename == "" or key_file.filename is None)
+            ):
+                return render_template("index.html", error="No selected file")
+            if pdf_file and cert_file:
+                pdf_filename = "upload.pdf"
+                key_filename = "key.pem"
+                cert_filename = "cert.pem"
+                pdf_file.save(
+                    os.path.join(app.config["UPLOAD_FOLDER"], pdf_filename)
+                )
+                key_file.save(
+                    os.path.join(app.config["UPLOAD_FOLDER"], key_filename)
+                )
+                cert_file.save(
+                    os.path.join(app.config["UPLOAD_FOLDER"], cert_filename)
+                )
+                signed_pdf_filename = sign_with_timestamp(
+                    "./files/" + pdf_filename,
+                    "./files/" + key_filename,
+                    "./files/" + cert_filename,
+                    tsa_url,
+                )
+                if signed_pdf_filename is None:
+                    return render_template(
+                        "signature.html", error="Error while signing"
+                    )
+                return render_template(
+                    "index.html",
+                    file_url=url_for(
+                        "download_file", filename=signed_pdf_filename
+                    ),
+                )
+    except Exception as e:
+        return render_template("index.html", error=str(e))
     return render_template("index.html")
 
 
 @app.route("/signature", methods=["POST", "GET"])
 def add_signature():
-    if request.method == "POST":
-        if (
-            "pdf_file" not in request.files
-            or "cert_file" not in request.files
-            or "key_file" not in request.files
-        ):
-            return render_template("signature.html", error="No file part")
-        pdf_file = request.files["pdf_file"]
-        key_file = request.files["key_file"]
-        cert_file = request.files["cert_file"]
-        if (
-            (pdf_file.filename == "" or pdf_file.filename is None)
-            or (cert_file.filename == "" or cert_file.filename is None)
-            or (key_file.filename == "" or key_file.filename is None)
-        ):
-            return render_template("signature.html", error="No selected file")
-        if pdf_file and cert_file:
-            pdf_filename = "upload.pdf"
-            key_filename = "key.pem"
-            cert_filename = "cert.pem"
-            pdf_file.save(
-                os.path.join(app.config["UPLOAD_FOLDER"], pdf_filename)
-            )
-            key_file.save(
-                os.path.join(app.config["UPLOAD_FOLDER"], key_filename)
-            )
-            cert_file.save(
-                os.path.join(app.config["UPLOAD_FOLDER"], cert_filename)
-            )
-            signed_pdf_filename = sign_without_timestamp(
-                "./files/" + pdf_filename,
-                "./files/" + key_filename,
-                "./files/" + cert_filename,
-            )
-            return render_template(
-                "signature.html",
-                file_url=url_for("download_file", filename=signed_pdf_filename),
-            )
+    try:
+        if request.method == "POST":
+            if (
+                "pdf_file" not in request.files
+                or "cert_file" not in request.files
+                or "key_file" not in request.files
+            ):
+                return render_template("signature.html", error="No file part")
+            pdf_file = request.files["pdf_file"]
+            key_file = request.files["key_file"]
+            cert_file = request.files["cert_file"]
+            if (
+                (pdf_file.filename == "" or pdf_file.filename is None)
+                or (cert_file.filename == "" or cert_file.filename is None)
+                or (key_file.filename == "" or key_file.filename is None)
+            ):
+                return render_template(
+                    "signature.html", error="No selected file"
+                )
+            if pdf_file and cert_file:
+                pdf_filename = "upload.pdf"
+                key_filename = "key.pem"
+                cert_filename = "cert.pem"
+                pdf_file.save(
+                    os.path.join(app.config["UPLOAD_FOLDER"], pdf_filename)
+                )
+                key_file.save(
+                    os.path.join(app.config["UPLOAD_FOLDER"], key_filename)
+                )
+                cert_file.save(
+                    os.path.join(app.config["UPLOAD_FOLDER"], cert_filename)
+                )
+                signed_pdf_filename = sign_without_timestamp(
+                    "./files/" + pdf_filename,
+                    "./files/" + key_filename,
+                    "./files/" + cert_filename,
+                )
+                if signed_pdf_filename is None:
+                    return render_template(
+                        "signature.html", error="Error while signing"
+                    )
+                return render_template(
+                    "signature.html",
+                    file_url=url_for(
+                        "download_file", filename=signed_pdf_filename
+                    ),
+                )
+    except Exception as e:
+        return render_template("signature.html", error=str(e))
     return render_template("signature.html")
 
 
 @app.route("/timestamp", methods=["POST", "GET"])
 def add_timestamp():
-    if request.method == "POST":
-        if "pdf_file" not in request.files:
-            return render_template("timestamp.html", error="No file part")
-        pdf_file = request.files["pdf_file"]
-        if pdf_file.filename == "" or pdf_file.filename is None:
-            return render_template("timestamp.html", error="No selected file")
-        if pdf_file:
-            pdf_filename = "upload.pdf"
-            pdf_file.save(
-                os.path.join(app.config["UPLOAD_FOLDER"], pdf_filename)
-            )
-            signed_pdf_filename = sign_only_timestamp(
-                "./files/" + pdf_filename,
-            )
-            return render_template(
-                "timestamp.html",
-                file_url=url_for("download_file", filename=signed_pdf_filename),
-            )
+    try:
+        if request.method == "POST":
+            if "pdf_file" not in request.files:
+                return render_template("timestamp.html", error="No file part")
+            if "tsa_url" not in request.values:
+                return render_template("timestamp.html", error="No tsa url")
+            pdf_file = request.files["pdf_file"]
+            tsa_url = request.values["tsa_url"]
+            if pdf_file.filename == "" or pdf_file.filename is None:
+                return render_template(
+                    "timestamp.html", error="No selected file"
+                )
+            if pdf_file:
+                pdf_filename = "upload.pdf"
+                pdf_file.save(
+                    os.path.join(app.config["UPLOAD_FOLDER"], pdf_filename)
+                )
+                signed_pdf_filename = sign_only_timestamp(
+                    "./files/" + pdf_filename,
+                    tsa_url,
+                )
+                if signed_pdf_filename is None:
+                    return render_template(
+                        "timestamp.html", error="Error while signing"
+                    )
+                return render_template(
+                    "timestamp.html",
+                    file_url=url_for(
+                        "download_file", filename=signed_pdf_filename
+                    ),
+                )
+    except Exception as e:
+        return render_template("timestamp.html", error=str(e))
     return render_template("timestamp.html")
 
 
--- sign.py
+++ sign.py
@@ -4,53 +4,65 @@ from pyhanko.pdf_utils.incremental_writer import IncrementalPdfFileWriter
 tsa_url = "https://freetsa.org/tsr"
 
 
-def sign_with_timestamp(pdf, key, cert):
-    cms_signer = signers.SimpleSigner.load(key, cert)
+def sign_with_timestamp(pdf, key, cert, tsa_url):
+    try:
+        cms_signer = signers.SimpleSigner.load(key, cert)
 
-    tst_client = timestamps.HTTPTimeStamper("https://freetsa.org/tsr")
+        tst_client = timestamps.HTTPTimeStamper(tsa_url)
 
-    with open(pdf, "rb") as inf:
-        if cms_signer:
-            with open("./files/signed.pdf", "wb") as outf:
-                w = IncrementalPdfFileWriter(inf)
-                signers.sign_pdf(
-                    w,
-                    signers.PdfSignatureMetadata(field_name="Signature1"),
-                    signer=cms_signer,
-                    timestamper=tst_client,
-                    output=outf,
-                )
-    return "signed.pdf"
+        with open(pdf, "rb") as inf:
+            if cms_signer:
+                with open("./files/signed.pdf", "wb") as outf:
+                    w = IncrementalPdfFileWriter(inf)
+                    signers.sign_pdf(
+                        w,
+                        signers.PdfSignatureMetadata(field_name="Signature1"),
+                        signer=cms_signer,
+                        timestamper=tst_client,
+                        output=outf,
+                    )
+        return "signed.pdf"
+    except Exception as e:
+        print(e)
+        return None
 
 
 def sign_without_timestamp(pdf, key, cert):
-    cms_signer = signers.SimpleSigner.load(key, cert)
+    try:
+        cms_signer = signers.SimpleSigner.load(key, cert)
 
-    with open(pdf, "rb") as inf:
-        if cms_signer:
-            with open("./files/signed_without_timestamp.pdf", "wb") as outf:
+        with open(pdf, "rb") as inf:
+            if cms_signer:
+                with open("./files/signed_without_timestamp.pdf", "wb") as outf:
+                    w = IncrementalPdfFileWriter(inf)
+                    signers.sign_pdf(
+                        w,
+                        signers.PdfSignatureMetadata(field_name="Signature1"),
+                        signer=cms_signer,
+                        output=outf,
+                    )
+        return "signed_without_timestamp.pdf"
+    except Exception as e:
+        print(e)
+        return None
+
+
+def sign_only_timestamp(pdf, tsa_url):
+    try:
+        tst_client = timestamps.HTTPTimeStamper(tsa_url)
+        with open(pdf, "rb") as inf:
+            with open("./files/only_timestamp.pdf", "wb") as outf:
                 w = IncrementalPdfFileWriter(inf)
-                signers.sign_pdf(
-                    w,
-                    signers.PdfSignatureMetadata(field_name="Signature1"),
-                    signer=cms_signer,
+                signer = signers.pdf_signer.PdfTimeStamper(
+                    timestamper=tst_client
+                )
+                signer.timestamp_pdf(
+                    md_algorithm="sha256",
+                    pdf_out=w,
+                    timestamper=tst_client,
                     output=outf,
                 )
-    return "signed_without_timestamp.pdf"
-
-
-def sign_only_timestamp(pdf):
-
-    tst_client = timestamps.HTTPTimeStamper("https://freetsa.org/tsr")
-
-    with open(pdf, "rb") as inf:
-        with open("./files/only_timestamp.pdf", "wb") as outf:
-            w = IncrementalPdfFileWriter(inf)
-            signer = signers.pdf_signer.PdfTimeStamper(timestamper=tst_client)
-            signer.timestamp_pdf(
-                md_algorithm="sha256",
-                pdf_out=w,
-                timestamper=tst_client,
-                output=outf,
-            )
-    return "only_timestamp.pdf"
+        return "only_timestamp.pdf"
+    except Exception as e:
+        print(e)
+        return None
